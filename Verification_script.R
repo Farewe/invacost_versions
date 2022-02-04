43e93f6431da4bc2071dab5d3e84f4d8fdb9e4f4
Boris Leroy <leroy.boris@gmail.com>
2022-02-04 10:33:20 +0100

    Updated to version 4.1

--- Verification_script.R
+++ Verification_script.R
@@ -121,3 +121,26 @@ invacost$Probable_ending_year_adjusted <- as.numeric(invacost$Probable_ending_ye
 
 dim(invacost)
 
+
+
+invacost <- read.csv2("InvaCost_4.1.csv", 
+                      sep = ";", header = TRUE,
+                      na.strings = c("NA", "#N/A", "#DIV/0!", "#VALEUR!", 
+                                     "#REF!",
+                                     "Unspecified", "unspecified", "Unknown", ""))
+
+# 
+
+invacost$Cost_estimate_per_year_original_currency <- as.numeric(invacost$Cost_estimate_per_year_original_currency)
+invacost$Cost_estimate_per_year_2017_USD_exchange_rate <- as.numeric(invacost$Cost_estimate_per_year_2017_USD_exchange_rate)
+invacost$Cost_estimate_per_year_2017_USD_PPP <- as.numeric(invacost$Cost_estimate_per_year_2017_USD_PPP)
+invacost$Applicable_year <- as.numeric(invacost$Applicable_year)
+invacost$Publication_year <- as.numeric(invacost$Publication_year)
+invacost$Probable_starting_year <- as.numeric(invacost$Probable_starting_year)
+invacost$Probable_ending_year <- as.numeric(invacost$Probable_ending_year)
+invacost$Probable_starting_year_adjusted <- as.numeric(invacost$Probable_starting_year_adjusted)
+invacost$Probable_ending_year_adjusted <- as.numeric(invacost$Probable_ending_year_adjusted)
+
+
+dim(invacost)
+
